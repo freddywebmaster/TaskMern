7b000a82f534adcab425a8a70a00303e46f3b61a
Freddy <freddy12gutierrez@gmail.com>
2021-07-11 14:41:08 -0600

    se cambio a hash router para evitar errores al recargar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/auth/Login';
 import NuevaCuenta from './components/auth/NuevaCuenta';
 import Proyectos from './components/proyectos/Proyectos';
